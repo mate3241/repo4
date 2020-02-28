d7912e2d722aaed299e291ef637e8d9d5222dff6
Balla Zsolt <ballatanar@gmail.com>
2020-02-28 10:52:50 +0100

    feladat2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log(2 + 3);
+function feladat2(number) { // min and max included 
+  for (let i = 0; i < number.length; i++) {
+    number[i] = Math.floor((Math.random() * 1000) + 1);
+  }
+};
+// let randNum = new Array(5);
+// console.log(genRandomNum(randNum));
+// console.log(randNum);
+
