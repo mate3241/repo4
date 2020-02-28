eee0174e80ba5d8b88a509b73d45542749ba2554
mate3241 <szabomatedenes@gmail.com>
2020-02-28 10:00:53 +0100

    feladat7

--- main.js
+++ main.js
@@ -20,3 +20,13 @@ const feladat5 = (array) => {
   array.sort((a, b) => { return a - b; });
   return array;
 };
+
+const feladat7 = (array) => {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0 && i % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+};
