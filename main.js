520306d310d8757ba44ad214cf3291b3ce25f854
mate3241 <szabomatedenes@gmail.com>
2020-02-28 09:51:53 +0100

    feladat3

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const feladat1 = (n) => {
-	let array = [];
-	for (let i = 0; i < array.length; i++) {
-		array.push(0);
-	}
-	return array;
+  let array = [];
+  for (let i = 0; i < array.length; i++) {
+    array.push(0);
+  }
+  return array;
+}
+
+const feladat3 = (array) => {
+  let evenNumbers = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      evenNumbers++;
+    }
+  }
+  return evenNumbers;
 }
