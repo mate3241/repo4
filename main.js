326c328e45defc685bdb08c22d7ba8a450d08057
mate3241 <szabomatedenes@gmail.com>
2020-02-28 10:05:59 +0100

    feladat9

--- main.js
+++ main.js
@@ -30,3 +30,13 @@ const feladat7 = (array) => {
   }
   return count;
 };
+
+const feladat9 = (array) => {
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+};
